05fdcf9dbb2bbd80b8dbb7c43d3a72b8d4027e41
JDLO <juandavidleono@gmail.com>
2026-05-30 16:57:27 +0200

    Create auhtenticationService

--- frontend-auth/src/app/services/auth.service.ts
+++ frontend-auth/src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginRequest } from '../models/LoginRequest';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:8080/api/auth';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(credentials: LoginRequest): Observable<any> {
+    return this.http.post(this.apiUrl + '/login', credentials);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('auth_token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('auth_token');
+  }
+
+  logout(): void {
+    localStorage.removeItem('auth_token');
+  }
 }
