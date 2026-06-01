7d2fc4a8947d583d831091a5ee57afa7c562ac39
JDLO <juandavidleono@gmail.com>
2026-06-01 18:51:58 +0200

    Added routing for SSO component

--- frontend-auth/src/app/app-routing.module.ts
+++ frontend-auth/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SsoCallbackComponent } from './components/sso-callback/sso-callback.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path:'', redirectTo:'login', pathMatch:'full' },
   { path:'login', component: LoginComponent },
+  { path:'sso/callback', component: SsoCallbackComponent },
   { path:'**', redirectTo:'login' },
 ];
 
