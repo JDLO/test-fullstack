9bf55972aac46151145b523a5de3d550667556b0
JDLO <juandavidleono@gmail.com>
2026-06-01 13:52:21 +0200

    Added import in app module of material select

--- frontend-auth/src/app/app.module.ts
+++ frontend-auth/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatIconModule,
     MatSnackBarModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
